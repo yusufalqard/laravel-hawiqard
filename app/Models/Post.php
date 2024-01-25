f16b602bd18cbf0707e3385c74f36aeb00930c1f
yusufalqard <yusuf.alqard@gmail.com>
2024-01-25 16:47:02 +0700

    add function guarded into model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    // protected $fillable = ['title','excerpt','body'];
+    // Alasannya menggunakan fillable ini cukup kompleks apalagi menginput banyak index baru
+    // Maka gunakan protected guarded
+    protected $guarded = ['id'];
+    //Hanya id yang dijaga tidak boleh diisi manual, selebihnya index tipe lain dapat diisi
 }
