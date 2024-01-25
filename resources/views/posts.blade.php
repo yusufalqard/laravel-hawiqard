a149f3fadf200db07c7ee5f1e834c39b6c0197d6
yusufalqard <yusuf.alqard@gmail.com>
2024-01-25 16:48:36 +0700

    fix code replacement

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,15 +1,15 @@
 @extends ('layouts.main')
 
 @section('container')
+<h1 class="mb-5">Halaman Posts</h1>
     @foreach ($posts as $post)
     <article class="mb-5">
         <h2>
-            <a href="/posts/{{ $post["slug"] }}">
-                {{ $post["title"] }}
+            <a href="/posts/{{ $post->id }}">
+                {{ $post->title }}
             </a>
         </h2>
-        <h5>By: {{ $post["author"] }}</h5>
-        <p>{{  $post["body"] }}</p>
+        <p>{{  $post->excerpt }}</p>
     </article>
     @endforeach
 @endsection
