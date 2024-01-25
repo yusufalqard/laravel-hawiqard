b5139a13e233129149bd63d0cf2b726e96d4a8e1
yusufalqard <yusuf.alqard@gmail.com>
2024-01-25 17:35:52 +0700

    add post category

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,7 +5,7 @@
     @foreach ($posts as $post)
     <article class="mb-5">
         <h2>
-            <a href="/posts/{{ $post->id }}">
+            <a href="/posts/{{ $post->slug }}">
                 {{ $post->title }}
             </a>
         </h2>
