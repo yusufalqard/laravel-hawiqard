182855b2bfd98067281f12de809a295660380668
yusufalqard <yusuf.alqard@gmail.com>
2024-02-05 09:19:32 +0700

    add seeder function

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,6 +20,59 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
+        // ]);
+        // User::create([
+        //     'name' => 'Hawialqard',
+        //     'email' => 'yusuf.alqard@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
+        User::factory(3)->create();
+
+        Category::create([
+            'name' => 'Web-Programming',
+            'slug' => 'web-programming'
+        ]);
+        Category::create([
+            'name' => 'Responsive-Web-Design',
+            'slug' => 'responsive-web-design'
+        ]);
+        Category::create([
+            'name' => 'Rest API',
+            'slug' => 'rest-api'
+        ]);
+        
+        Post::factory(10)->create();
+        // Post::create([
+        //     'title' => 'Tips menjadi Front-End Developer',
+        //     'slug' => 'tips-front-end-dev',
+        //     'excerpt' => 'Untuk menjadi Front-End Dev, yang perlu dipersiapkan adalah ...',
+        //     'body' => 'Untuk menjadi Front-End Dev, yang perlu dipersiapkan adalah HTML, CSS, Javascript sebagai acuan ilmu dasar. Lalu diikuti dengan framework seperti Bootstrap/ReactJS/Vue.js mempermudah pekerjaan saat membangun website',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
+        // Post::create([
+        //     'title' => 'Tips membuat website responsive',
+        //     'slug' => 'tips-responsive-web',
+        //     'excerpt' => 'Untuk membuat responsive website, yang perlu dipersiapkan adalah ...',
+        //     'body' => 'Untuk membuat responsive website, yang perlu dipersiapkan adalah framework favorit seperti Bootstrap, Svalte, Vue.js, ReactJS, dsb',
+        //     'category_id' => 2,
+        //     'user_id' => 1
+        // ]);
+        // Post::create([
+        //     'title' => 'Tip membuat aplikasi web API',
+        //     'slug' => 'tips-aplikasi-web-api',
+        //     'excerpt' => 'Setelah anda membangun website, langkah selanjutnya adalah ...',
+        //     'body' => 'Setelah anda membangun website, langkah selanjutnya adalah setelah membangun keamanan back-end software maka kita harus melakukan pembuatan API dengan tujuan memastikan aplikasi dapat berjalan optimal dari segi pihak client terhadap main server',
+        //     'category_id' => 3,
+        //     'user_id' => 1
+        // ]);
+        // Post::create([
+        //     'title' => 'Tip payment gateway dengan API',
+        //     'slug' => 'tips-gateway-payment-web-api',
+        //     'excerpt' => 'Jika website sudah mencakupi dari ...',
+        //     'body' => 'Jika website sudah mencakupi dari Design Template Website, Proteksi Autentikasi Keamanan Server Web Hosting, dan Interaksi API yang digunakan oleh Client. Maka yang kita lakukan membangun sebuah Gateway Payment API untuk pembayaran donasi dari client sebagai tanda terima kasih terhadap kepedulian kritik dan saran membuat website lebih baik ke-depannya',
+        //     'category_id' => 3,
+        //     'user_id' => 1
         // ]);
     }
 }
