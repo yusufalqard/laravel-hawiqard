a2b4735b27d2a361d6c07f08824fa77f5f943ba2
yusufalqard <yusuf.alqard@gmail.com>
2024-02-05 09:22:15 +0700

    update post blade function for specific post and some error need fixed

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,8 +3,9 @@
 @section('container')
     <article>
         <h2 class ="mb-5">{{ $post->title }}</h2>
+        <p> By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
         {!! $post->body !!}
     </article>
 
-    <a href="/posts">Kembali ke Halaman Posts</a>
+    <a href="/posts" class="d-block mt-3">Kembali ke Halaman Posts</a>
 @endsection
