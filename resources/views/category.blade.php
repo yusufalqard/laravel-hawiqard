4a1dca41fa6d2dcb07f1ff1ba77b32868d05e07d
yusufalqard <yusuf.alqard@gmail.com>
2024-04-15 21:48:28 +0700

    add image generator for card posts

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -2,16 +2,28 @@
 
 @section('container')
 <h1 class="mb-5">Post Category :  {{ $category }}</h1>
-
-    @foreach ($posts as $post)
+<div class="container">
+    <div class="row">
+        @foreach ($posts as $post)
+        <div class="col-md-3 mb-3">
     <article class="mb-5">
         <h2>
-            <a href="/posts/{{ $post->slug }}">
+            <div class="card">
+                <img src="https://source.unsplash.com/500x300?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
+            <a href="/posts/{{ $post->slug }}" class="text-dark text-decoration-none">
                 {{ $post->title }}
             </a>
+            </div>
         </h2>
-        <p>{{  $post->excerpt }}</p>
+        <p class="card-text">
+        <small class="text-body-secondary">
+            {{  $post->excerpt }}
+            </small>
+        </p>
     </article>
-    @endforeach
+</div>
+            @endforeach
+    </div>
+</div>
 @endsection
 
