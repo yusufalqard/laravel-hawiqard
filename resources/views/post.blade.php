4cc6efb6f5fe970302576950ff3f858e981e5208
yusufalqard <yusuf.alqard@gmail.com>
2024-04-15 21:50:53 +0700

    add image generator for card post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,11 +1,18 @@
 @extends('layouts.main')
 
 @section('container')
-    <article>
-        <h2 class ="mb-5">{{ $post->title }}</h2>
-        <p> By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
-        {!! $post->body !!}
-    </article>
-
-    <a href="/posts" class="d-block mt-3">Kembali ke Halaman Posts</a>
+    <div class="container">
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-5">
+                <h2 class ="mb-3">{{ $post->title }}</h2>
+                <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }} </a> in <a href="/categories/{{ $post->category->slug }} " class="text-decoration-none">{{ $post->category->name }} </a>{{ $post->created_at->diffForHumans() }}</p>
+                <img src="https://source.unsplash.com/500x300?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
+                <article class="my-3 fs-5">
+                    {!! $post->body !!}
+                </article>
+                <a href="/posts" class="d-block mt-3">Kembali ke Halaman Posts</a>
+            </div>
+        </div>
+    </div>
+    
 @endsection
