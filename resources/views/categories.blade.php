696f4899228ce397291378b6ce3a1d29514d7be0
yusufalqard <yusuf.alqard@gmail.com>
2024-04-16 11:42:09 +0700

    optimize code

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -2,18 +2,21 @@
 
 @section('container')
 <h1 class="mb-5">Post Categories </h1>
-
-    @foreach ($categories as $category)
-    <ul>
-        <li>
-        <h2>
-            <a href="/categories/{{ $category->slug }}">
-                {{ $category->name }}
-            </a>
-        </h2>
-        </li>
-    </ul>
-    </article>
-    @endforeach
+    <div class="container">
+        <div class="row">
+            @foreach ($categories as $category)
+            <div class="col-md-4">
+                <a href="/categories/{{ $category->slug }}">
+                <div class="card text-bg-dark">
+                    <img src="https://source.unsplash.com/500x500?{{ $category->name }}" class="card-img" alt="{{ $category->name }}">
+                    <div class="card-img-overlay d-flex align-items-center p-0">
+                      <h5 class="card-title text-center flex-fill text-light bg-dark p-4">{{ $category->name }}</h5>
+                    </div>
+                  </div>
+                </a>
+            </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
 
